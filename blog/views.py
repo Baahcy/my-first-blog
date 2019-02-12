7cda8d034a68551d90be5edd9f667ed61b50be57
Cyril <cbaah123@gmail.com>
2019-02-12 14:35:33 +0000

    adding a download link

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 
 def index(request):
+    #posts = Post.objects.filter(publisher_date__lte = timezone.now()).order_by('published_date')
     return render(request,'blog/index.html',{})
+
